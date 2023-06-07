d7599c3ae117db31e4e356161f19e703ddf6edf5
Evistix <mephistakaiwan@gmail.com>
2023-06-07 12:58:14 +0200

    gateway + eureka

---
+++ src/main/java/com/evi/teamfindernotifications/TeamFinderNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.evi.teamfindernotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFinderNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamFinderNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evi/teamfindernotifications/TeamFinderNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evi.teamfindernotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFinderNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
