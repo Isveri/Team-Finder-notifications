c1922ace30514f59280799ea74b5a2caace0b7ed
Evistix <mephistakaiwan@gmail.com>
2023-07-05 14:30:23 +0200

    email verification

--- src/main/java/com/evi/teamfindernotifications/messaging/converter/NotificationMessageConverter.java
+++ src/main/java/com/evi/teamfindernotifications/messaging/converter/NotificationMessageConverter.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,7 @@ import javax.jms.TextMessage;
 
 
 @Component
+@ConditionalOnProperty(prefix = "notification", name = "service",havingValue = "activemq")
 public class NotificationMessageConverter implements MessageConverter {
 
     private static final Logger LOGGER =
